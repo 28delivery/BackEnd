f0a5ce4dab58835cb3da2d62d97b6ab866b3fb57
Imnotcoderdude <topsolo7@naver.com>
2025-02-13 23:06:06 +0900

    init commit

---
+++ src/test/java/com/sparta/spring_deep/_delivery/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_deep._delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
