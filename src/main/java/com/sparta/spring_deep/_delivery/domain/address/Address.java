e93fbaf5718be573d411b54bc1d487547bfd571c
Imnotcoderdude <topsolo7@naver.com>
2025-02-17 12:10:06 +0900

    🩹 fix : 잘못된 필드, 테이블명 수정 - BaseEntity ~_id 제거 - Address 테이블명 지정

--- src/main/java/com/sparta/spring_deep/_delivery/domain/address/Address.java
+++ src/main/java/com/sparta/spring_deep/_delivery/domain/address/Address.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import org.hibernate.annotations.UuidGenerator;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "p_address")
 public class Address extends BaseEntity {
 
     @Id
