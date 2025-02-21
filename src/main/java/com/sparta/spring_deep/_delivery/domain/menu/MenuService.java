a3da3ddf53d68abeef9dac689b7164bf9b04e312
Imnotcoderdude <topsolo7@naver.com>
2025-02-21 14:49:05 +0900

    ♻️ refactor : 중복 코드 메서드로 추출 후 AuthTools 저장

--- src/main/java/com/sparta/spring_deep/_delivery/domain/menu/MenuService.java
+++ src/main/java/com/sparta/spring_deep/_delivery/domain/menu/MenuService.java
@@ -7,8 +7,8 @@ import com.sparta.spring_deep._delivery.domain.restaurant.Restaurant;
 import com.sparta.spring_deep._delivery.domain.restaurant.RestaurantRepository;
 import com.sparta.spring_deep._delivery.domain.user.details.UserDetailsImpl;
 import com.sparta.spring_deep._delivery.domain.user.entity.User;
-import com.sparta.spring_deep._delivery.domain.user.entity.UserRole;
 import com.sparta.spring_deep._delivery.domain.user.repository.UserRepository;
+import com.sparta.spring_deep._delivery.util.AuthTools;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -18,10 +18,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -49,11 +47,7 @@ public class MenuService {
             .orElseThrow(() -> new EntityNotFoundException(
                 "메뉴 추가 : 해당하는 레스토랑이 없습니다. restaurantId= " + restaurantId));
 
-        if (!restaurant.getOwner().getUsername().equals(userDetails.getUser().getUsername())
-            && userDetails.getUser().getRole() != UserRole.ADMIN) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-                "메뉴 추가 : 메뉴를 추가할 권한이 없습니다. 현재 권한 : " + userDetails.getUser().getRole());
-        }
+        AuthTools.roleCheck(userDetails, restaurant, "메뉴 추가");
 
         Menu menu = Menu.builder()
             .restaurantId(restaurant)
@@ -102,11 +96,8 @@ public class MenuService {
             .orElseThrow(() -> new EntityNotFoundException(
                 "메뉴 수정 : 해당하는 레스토랑이 없습니다. restaurantId : " + menu.getRestaurantId()));
 
-        if (!restaurant.getOwner().getUsername().equals(userDetails.getUser().getUsername())
-            && userDetails.getUser().getRole() != UserRole.ADMIN) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-                "메뉴 수정 : 메뉴를 수정할 권한이 없습니다. 현재 권한 : " + userDetails.getUser().getRole());
-        }
+        AuthTools.roleCheck(userDetails, restaurant, "메뉴 수정");
+
         menu.update(
             requestDto.getName(),
             requestDto.getDescription(),
@@ -135,14 +126,8 @@ public class MenuService {
             .orElseThrow(() -> new EntityNotFoundException(
                 "메뉴 삭제 : 해당 유저를 찾을 수 없습니다. username : " + userDetails.getUser().getUsername()));
 
-        log.info("메뉴 삭제 : 현재 유저 권한 : " + user.getRole());
-        log.info(restaurant.getOwner() + " = " + userDetails.getUser() + " ?");
-        if (!restaurant.getOwner().getUsername().equals(userDetails.getUser().getUsername())
-            && userDetails.getUser().getRole() != UserRole.ADMIN) {
-            log.error("메뉴 삭제 검증 로직 내부 : 현재 유저 권한 : " + user.getRole());
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-                "메뉴 삭제 : 메뉴를 삭제할 권한이 없습니다. 현재 권한 : " + userDetails.getUser().getRole());
-        }
+        AuthTools.roleCheck(userDetails, restaurant, "메뉴 삭제");
+
         log.info("메뉴 삭제 됨 : 현재 유저 권한 : " + user.getRole());
         menu.delete(user);
 
@@ -159,8 +144,13 @@ public class MenuService {
 
         User user = userRepository.findById(userDetails.getUser().getUsername())
             .orElseThrow(() -> new EntityNotFoundException(
-                "메뉴 AI 설명 생성 : 해당 유저를 찾을 수 없습니다. username : " + userDetails.getUser()
-                    .getUsername()));
+                "AI 설명 생성 : 해당 유저를 찾을 수 없습니다. username : " + userDetails.getUser().getUsername()));
+
+        Restaurant restaurant = restaurantRepository.findById(menu.getRestaurantId().getId())
+            .orElseThrow(() -> new EntityNotFoundException(
+                "AI 설명 생성 : 해당하는 레스토랑이 없습니다. restaurantId= " + menu.getRestaurantId()));
+
+        AuthTools.roleCheck(userDetails, restaurant, "Ai 설명 생성");
 
         String aiDescription = googleAiService.createAiDescription(menu.getDescription());
 
@@ -169,4 +159,5 @@ public class MenuService {
         aiRepository.save(aiLog);
         return new MenuAiResponseDto(menuId, aiDescription);
     }
+
 }
---
+++ src/main/java/com/sparta/spring_deep/_delivery/util/AuthTools.java
@@ -0,0 +1,47 @@
+package com.sparta.spring_deep._delivery.util;
+
+import com.sparta.spring_deep._delivery.domain.restaurant.Restaurant;
+import com.sparta.spring_deep._delivery.domain.user.details.UserDetailsImpl;
+import com.sparta.spring_deep._delivery.domain.user.entity.UserRole;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class AuthTools {
+
+    public static void roleCheck(UserDetailsImpl userDetails, Restaurant restaurant,
+        String msg) {
+
+        if (userDetails.getUser().getRole().equals(UserRole.OWNER)) {
+            ownerCheck(userDetails, restaurant, msg);
+        } else if (userDetails.getUser().getRole().equals(UserRole.MANAGER)) {
+//            ManagerCheck(userDetails, restaurant, msg);
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "현재 매니저가 없습니다.");
+        }
+
+    }
+
+    // 오너 이름 일치 검사
+    public static void ownerCheck(UserDetailsImpl userDetails, Restaurant restaurant, String msg) {
+
+        if (!restaurant.getOwner().getUsername().equals(userDetails.getUser().getUsername())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                "(" + msg + ") 오너가 일치하지 않습니다. 요청된 Owner : (" + userDetails.getUser().getUsername()
+                    + ") 등록된 Owner : (" + restaurant.getOwner().getUsername() + ")");
+        }
+
+    }
+
+    // 매니저 이름 일치 검사.
+    // 현재 레스토랑에 매니저를 사용하지 않음
+//    public static void managerCheck(UserDetailsImpl userDetails, Restaurant restaurant,
+//        String msg) {
+//
+//        if (!restaurant.getManager().getUsername().equals(userDetails.getUser().getUsername())) {
+//            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+//                "(" + msg + ") 매니저가 일치하지 않습니다. 요청된 Manager : (" + userDetails.getUser()
+//                    .getUsername()
+//                    + ") 등록된 Manager : (" + restaurant.getManager().getUsername() + ")");
+//        }
+//    }
+
+}
