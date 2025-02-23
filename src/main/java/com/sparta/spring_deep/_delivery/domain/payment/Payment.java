e177c02d0166338ee3653544d338e843b069257e
sohyun119 <sh-119@naver.com>
2025-02-24 02:36:28 +0900

    ✨ feat : Payment API 생성

--- src/main/java/com/sparta/spring_deep/_delivery/domain/payment/Payment.java
+++ src/main/java/com/sparta/spring_deep/_delivery/domain/payment/Payment.java
@@ -16,11 +16,13 @@ import java.math.BigDecimal;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 @Entity
 @Getter
+@Setter
 @RequiredArgsConstructor
 public class Payment extends BaseEntity {
 
@@ -29,7 +31,7 @@ public class Payment extends BaseEntity {
     private UUID id;
 
     @OneToOne
-    @JoinColumn(name = "order_id", nullable = false)
+    @JoinColumn(name = "order_id", nullable = false, unique = true)
     private Order order;
 
     @JoinColumn(name = "amount", nullable = false)
@@ -45,6 +47,26 @@ public class Payment extends BaseEntity {
     @Column(name = "payment_status", nullable = false, columnDefinition = "p_payment_method_enum")
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     private PaymentStatusEnum paymentStatus;
+
+    public Payment(String username, Order order, BigDecimal amount) {
+        super(username);
+        this.paymentMethod = PaymentMethodEnum.CARD;
+        this.paymentStatus = PaymentStatusEnum.PENDING;
+        this.order = order;
+        this.amount = amount;
+    }
+
+    public void completePayment() {
+        this.paymentStatus = PaymentStatusEnum.COMPLETED;
+    }
+
+    public void cancelPayment() {
+        this.paymentStatus = PaymentStatusEnum.CANCELED;
+    }
+
+    public void failPayment() {
+        this.paymentStatus = PaymentStatusEnum.FAILED;
+    }
 
     public enum PaymentMethodEnum {
         CARD
--- src/main/java/com/sparta/spring_deep/_delivery/domain/payment/PaymentController.java
+++ src/main/java/com/sparta/spring_deep/_delivery/domain/payment/PaymentController.java
@@ -1,7 +1,18 @@
 package com.sparta.spring_deep._delivery.domain.payment;
 
+import com.sparta.spring_deep._delivery.domain.user.details.UserDetailsImpl;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +21,60 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
     private final PaymentService paymentService;
+
+    // 결제 생성
+    @PostMapping("/payment")
+    public ResponseEntity<Payment> createPayment(@RequestBody PaymentRequestDto requestDto
+        , @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        Payment payment = paymentService.createPayment(userDetails.getUsername(),
+            UUID.fromString(requestDto.orderId),
+            requestDto.amount);
+        return ResponseEntity.status(HttpStatus.OK).body(payment);
+    }
+
+    // 결제 완료 처리
+    @PutMapping("/payment/complete")
+    public ResponseEntity<Payment> completePayment(@RequestParam String paymentId) {
+        
+        Payment payment = paymentService.completePayment(UUID.fromString(paymentId));
+        return ResponseEntity.status(HttpStatus.OK).body(payment);
+    }
+
+    // 결제 취소 처리
+    @PutMapping("/payment/cancel")
+    public ResponseEntity<Payment> cancelPayment(@RequestParam String paymentId) {
+
+        Payment payment = paymentService.cancelPayment(UUID.fromString(paymentId));
+        return ResponseEntity.status(HttpStatus.OK).body(payment);
+    }
+
+    // 결제 실패 처리
+    @PutMapping("/payment/fail")
+    public ResponseEntity<Payment> failPayment(@RequestParam String paymentId) {
+
+        Payment payment = paymentService.failPayment(UUID.fromString(paymentId));
+        return ResponseEntity.status(HttpStatus.OK).body(payment);
+    }
+
+    // 결제 조회
+    @GetMapping("/payment")
+    public ResponseEntity<Payment> getPayment(@AuthenticationPrincipal UserDetailsImpl userDetails,
+        @RequestParam String paymentId) {
+
+        Payment payment = paymentService.getPayment(userDetails.getUser(),
+            UUID.fromString(paymentId));
+        return ResponseEntity.status(HttpStatus.OK).body(payment);
+    }
+
+    // 결제 내역 삭제
+    @DeleteMapping("/payment")
+    public ResponseEntity<String> deletePayment(
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @RequestParam String paymentId) {
+
+        return paymentService.deletPayment(userDetails.getUser(), UUID.fromString(paymentId));
+    }
 
 
 }
